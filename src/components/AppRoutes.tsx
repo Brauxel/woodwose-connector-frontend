0907ee7a0020bac27728df838ff7221359fcd218
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-08-26 22:31:22 +1000

    [Aakash Bhatia]-[Setup WooCommerce Inventory page]-[Create woocommerce page]

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './Home'
+import { WooCommerceInventory } from './WooCommerceInventory'
 
 export const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/home" element={<Home />} />
+    <Route path="/woocommerce-inventory" element={<WooCommerceInventory />} />
   </Routes>
 )
---
+++ src/components/WooCommerceInventory/index.tsx
@@ -0,0 +1,1 @@
+export const WooCommerceInventory = () => <>WooCommerce Inventory</>
