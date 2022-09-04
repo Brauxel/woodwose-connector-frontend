57cab2831ca74d06e3b22380fc1ed580e81ab9db
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2022-09-04 21:42:18 +1000

    [Aakash Bhatia]-[Setup WooCommerce Inventory page]-[dynamic rendering of table]

--- src/components/pages/WooCommerceInventory/index.tsx
+++ src/components/pages/WooCommerceInventory/index.tsx
@@ -3,165 +3,172 @@ import { TableHead } from '../../shared/atoms/TableHead'
 import { Table } from '../../shared/elements/Table'
 import { TableRow } from '../../shared/molecules/TableRow'
 
+const products = [
+  {
+    name: "Men's Bamboo Tee- Dark Grey T-shirt",
+    id: 5015,
+    variations: [
+      {
+        id: 5020,
+        sku: 'BAMTSDG02--S',
+        price: '899',
+        size: 'S',
+        stock_quantity: 1,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=s',
+      },
+      {
+        id: 5019,
+        sku: 'BAMTSDG02--M',
+        price: '899',
+        size: 'M',
+        stock_quantity: 2,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=m',
+      },
+      {
+        id: 5018,
+        sku: 'BAMTSDG02--L',
+        price: '999',
+        size: 'L',
+        stock_quantity: 1,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=l',
+      },
+      {
+        id: 5017,
+        sku: 'BAMTSDG02--XL',
+        price: '899',
+        size: 'XL',
+        stock_quantity: 2,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=xl',
+      },
+      {
+        id: 5016,
+        sku: 'BAMTSDG02--XXL',
+        price: '1099',
+        size: 'XXL',
+        stock_quantity: 3,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=xxl',
+      },
+    ],
+  },
+  {
+    name: "Women's Bamboo Tank Top-Dark Grey",
+    id: 5030,
+    variations: [
+      {
+        id: 5031,
+        sku: 'BAMTSDG02--S',
+        price: '899',
+        size: 'S',
+        stock_quantity: 1,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=s',
+      },
+      {
+        id: 5032,
+        sku: 'BAMTSDG02--M',
+        price: '899',
+        size: 'M',
+        stock_quantity: 2,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=m',
+      },
+      {
+        id: 5033,
+        sku: 'BAMTSDG02--L',
+        price: '999',
+        size: 'L',
+        stock_quantity: 1,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=l',
+      },
+      {
+        id: 5034,
+        sku: 'BAMTSDG02--XL',
+        price: '899',
+        size: 'XL',
+        stock_quantity: 2,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=xl',
+      },
+      {
+        id: 5035,
+        sku: 'BAMTSDG02--XXL',
+        price: '1099',
+        size: 'XXL',
+        stock_quantity: 3,
+        permalink:
+          'https://woodwose.in/product/mens-bamboo-dark-grey-t-shirt/?attribute_pa_size=xxl',
+      },
+    ],
+  },
+]
+
 export const WooCommerceInventory = () => {
   return (
     <>
       <Table>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>SKU</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Size</TableHead>
-          <TableHead>Link</TableHead>
-        </TableRow>
+        <thead>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>SKU</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Size</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead>Link</TableHead>
+          </TableRow>
+        </thead>
+        {products.map((product) => {
+          return (
+            <tbody key={product.id}>
+              <TableRow key={product.variations[0].id}>
+                <TableColumn rowSpan={product.variations.length}>
+                  {product.name}
+                </TableColumn>
+                <TableColumn>{product.variations[0].sku}</TableColumn>
+                <TableColumn>₹{product.variations[0].price}</TableColumn>
+                <TableColumn>{product.variations[0].size}</TableColumn>
+                <TableColumn>
+                  {product.variations[0].stock_quantity}
+                </TableColumn>
+                <TableColumn>
+                  <a
+                    href={product.variations[0].permalink}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Click here to view the product on our website
+                  </a>
+                </TableColumn>
+              </TableRow>
 
-        <TableRow>
-          <TableColumn rowSpan={5}>
-            Men&apos;s Bamboo Tee- Dark Grey T-shirt
-          </TableColumn>
-          <TableColumn rowSpan={5}>BAMTSDG02</TableColumn>
-          <TableColumn>₹569</TableColumn>
-          <TableColumn>S</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/mens-100-organic-cotton-black-t-shirt/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹569</TableColumn>
-          <TableColumn>M</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/mens-100-organic-cotton-black-t-shirt/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹569</TableColumn>
-          <TableColumn>L</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/mens-100-organic-cotton-black-t-shirt/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹569</TableColumn>
-          <TableColumn>XL</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/mens-100-organic-cotton-black-t-shirt/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹569</TableColumn>
-          <TableColumn>XXL</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/mens-100-organic-cotton-black-t-shirt/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn rowSpan={5}>
-            Women&apos;s Bamboo Tank Top-Dark Grey
-          </TableColumn>
-          <TableColumn rowSpan={5}>BAWTTDG02</TableColumn>
-          <TableColumn>₹699</TableColumn>
-          <TableColumn>S</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/womens-dark-grey-tank-top/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹699</TableColumn>
-          <TableColumn>M</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/womens-dark-grey-tank-top/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹699</TableColumn>
-          <TableColumn>L</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/womens-dark-grey-tank-top/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹699</TableColumn>
-          <TableColumn>XL</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/womens-dark-grey-tank-top/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
-
-        <TableRow>
-          <TableColumn>₹699</TableColumn>
-          <TableColumn>XXL</TableColumn>
-          <TableColumn>
-            <a
-              href="https://woodwose.in/product/womens-dark-grey-tank-top/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Click here to view the product on our website
-            </a>
-          </TableColumn>
-        </TableRow>
+              {product.variations.map((variation, index) => {
+                if (index === 0) return ''
+                return (
+                  <TableRow key={variation.id}>
+                    <TableColumn>{variation.sku}</TableColumn>
+                    <TableColumn>{variation.price}</TableColumn>
+                    <TableColumn>{variation.size}</TableColumn>
+                    <TableColumn>{variation.stock_quantity}</TableColumn>
+                    <TableColumn>
+                      <a
+                        href={variation.permalink}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Click here to view the product on our website
+                      </a>
+                    </TableColumn>
+                  </TableRow>
+                )
+              })}
+            </tbody>
+          )
+        })}
       </Table>
     </>
   )
